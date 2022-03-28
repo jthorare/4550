af8500c9bfd466e9dcd9153d7f70b3aa8b8b6aa5
jthorare <joi.thorare@gmail.com>
2022-03-28 13:41:57 -0400

    styled new tuit

--- react/web-dev/src/components/tuiter/HomeScreen/WhatsHappening.js
+++ react/web-dev/src/components/tuiter/HomeScreen/WhatsHappening.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import { useDispatch } from "react-redux";
+import "./new-tuit.css"
 
 const WhatsHappening = () => {
   let [whatsHappening, setWhatsHappening]
@@ -10,15 +11,26 @@ const WhatsHappening = () => {
     console.log(whatsHappening);
   }
   return (
-    <>
-      <textarea value={whatsHappening}
-               onChange={(event) =>
-        setWhatsHappening(event.target.value)}>
-      </textarea>
-      <button onClick={tuitClickHandler}>
-        Tuit
-      </button>
-    </>
+    <div className="pb-2">
+      <div className="list-group list-group-horizontal">
+        <img className="list-group-item bg-transparent border-0 rounded-circle self-img" src="/tuiter/assets/tesla.png"/>
+        <textarea className="list-group-item bg-transparent wd-new-tuit bg-transparent border-0 w-100 pos-relative-nudge-up-right" 
+        value={whatsHappening}
+                placeholder="What's happening?"
+                onChange={(event) =>
+          setWhatsHappening(event.target.value)}>
+        </textarea>
+      </div>
+      <div className="list-group list-group-horizontal">
+        <i className="fa-regular fa-image list-group-item bg-transparent border-0"/>
+        <i className="fa-solid fa-arrow-trend-up list-group-item bg-transparent border-0"/>
+        <i className="fa-regular fa-face-smile list-group-item bg-transparent border-0"/>
+        <i className="fa-regular fa-calendar list-group-item bg-transparent border-0"/>
+        <button className="btn btn-primary btn-block rounded-pill" onClick={tuitClickHandler}>
+          Tuit
+        </button>
+      </div>
+    </div>
   );
 }
 export default WhatsHappening;
--- react/web-dev/src/components/tuiter/TuitList/TuitListItem.js
+++ react/web-dev/src/components/tuiter/TuitList/TuitListItem.js
@@ -33,25 +33,24 @@ const TuitListItem = (
     };
     return(
         <>
-
-        <div className="wd-home-bookmark list-group-item">
-            <i onClick={() => deleteTuit(tuit)} className="fas fa-remove fa-2x fa-pull-right"/>
-            <img className="wd-home-avatar" src={tuit["avatar-image"]}/>
-            <div className="wd-home-user-info">
-                <p className="wd-home-username">{tuit.postedBy.username}</p>
-                <p className="wd-home-handle">@{tuit.handle}</p>
-                <p className="wd-home-timestamp">- {tuit.time}</p>
+            <div className="wd-home-bookmark list-group-item">
+                <i onClick={() => deleteTuit(tuit)} className="fas fa-remove fa-2x fa-pull-right"/>
+                <img className="wd-home-avatar" src={tuit["avatar-image"]}/>
+                <div className="wd-home-user-info">
+                    <p className="wd-home-username">{tuit.postedBy.username}</p>
+                    <p className="wd-home-handle">@{tuit.handle}</p>
+                    <p className="wd-home-timestamp">- {tuit.time}</p>
+                </div>
+                <div className="wd-home-content">
+                    <p>{tuit.tuit}</p>
+                </div>
+                <div className="wd-home-snapshot">
+                    {tuit.attachments && tuit.attachments.image && <img className="wd-home-image" src={tuit.attachments.image}/>}
+                    {tuit.attachments && tuit.attachments.video && <video className="wd-home-image" src={tuit.attachments.video}/>}
+                    {tuit.title && <div className="wd-home-snapshot-title">{tuit.title}</div> }
+                </div>
+            <TuitStats tuit={tuit}/> 
             </div>
-            <div className="wd-home-content">
-                <p>{tuit.tuit}</p>
-            </div>
-            <div className="wd-home-snapshot">
-                {tuit.attachments && tuit.attachments.image && <img className="wd-home-image" src={tuit.attachments.image}/>}
-                {tuit.attachments && tuit.attachments.video && <video className="wd-home-image" src={tuit.attachments.video}/>}
-                {tuit.title && <div className="wd-home-snapshot-title">{tuit.title}</div> }
-            </div>
-           <TuitStats tuit={tuit}/> 
-        </div>
         </>
     )
 }
