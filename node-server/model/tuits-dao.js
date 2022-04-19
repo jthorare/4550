4ee3126fabd42a6c5da811bd79c9fe08bf7820b9
jthorare <joi.thorare@gmail.com>
2022-04-19 11:23:28 -0400

    fixing controller

--- node-server/model/tuits-dao.js
+++ node-server/model/tuits-dao.js
@@ -1,7 +1,5 @@
 import tuitsModel from './tuits-model.js';
-const findAllTuits = () => tuitsModel.find();
-const createTuit = (tuit) => tuitsModel.create(tuit);
-const deleteTuit = (tid) => tuitsModel.deleteOne({_id: tid});
-const updateTuit = (tid, tuit) => tuitsModel.updateOne({_id: tid}, {$set: tuit})
-
-export default {findAllTuits, createTuit, deleteTuit, updateTuit};
+export const findAllTuitsDao = () => tuitsModel.find();
+export const createTuitDao = (tuit) => tuitsModel.create(tuit);
+export const deleteTuitDao = (tid) => tuitsModel.deleteOne({_id: tid});
+export const updateTuitDao = (tid, tuit) => tuitsModel.updateOne({_id: tid}, {$set: tuit})
